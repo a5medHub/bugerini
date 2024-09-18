5f497306a296b3ab758d573a6d3df039288eba5c
a5medHub <ayassine.auce@gmail.com>
2024-09-18 11:57:31 +0300

    cleaning code display

--- index.js
+++ index.js
@@ -1,109 +1,81 @@
-import { menuArray } from "./data.js"
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import {
+    menuArray
+} from "./data.js"
+import {
+    v4 as uuidv4
+} from 'https://jspm.dev/uuid';
 
 const menuEl = document.getElementById("menu")
 const paymentEl = document.getElementById("payment")
 const totalEl = document.getElementById("total")
-const allData = []
-let totalPriceListCounter = 0
-// let theHtmlRender = []
-let priceOfId = {}
 const testBtn = document.getElementById("test")
-
+const allData = []
+const priceOfId = {}
+let totalPriceListCounter = 0
 
 document.addEventListener("click", (event) => {
     if (event.target.dataset.icon) {
         addToMenu(event.target.dataset.icon)
-       
+
     } else if (event.target.dataset.delete) {
         removeItemFromOrder(event.target.dataset.delete)
     }
 })
 
-function addToMenu(e, counter){
-    let id = uuidv4()
-    const addItem = menuArray.filter(item=> item.id == e )[0] 
-    let theHtmlRender =`
-            <div class="orders" id="${id}">
-                <div class="itemAdded">
-                    <h3>${addItem.name} <!--<small class="${addItem.name}">x${counter}</small>--></h3>
-                    <button class='delete' data-delete="${id}" >remove</button>
-
-                    <h5>$${addItem.price}</h5>
-                </div>
-            </div>`
-            priceOfId[id] = addItem.price
-            // console.log(id)
-            // console.log(priceOfId)
-            paymentEl.innerHTML += theHtmlRender
-            
-            totalAmmount(id, addItem.price, addItem.name, addItem.id)
-
-
-    
-}
-
 testBtn.addEventListener("click", (event) => {
-    // console.log(allData)
-    let numberX = Math.floor(Math.random(allData)*allData.length)
-    // console.log(numberX)
-    // console.log(allData[0])
-    const randomData = allData.map((e)=>{
+    let numberX = Math.floor(Math.random(allData) * allData.length)
+    const randomData = allData.map((e) => {
         return e[0]
     })
-    console.log("random number to value",numberX,randomData[numberX])
-    // allData.forEach((e)=>{
-        
-    //         console.log(e[numberX])
-    //     // console.log(e[0])
-    // })
+    console.log("random number to value", numberX, randomData[numberX])
 })
 
-function removeItemFromOrder(id){
+function removeItemFromOrder(id) {
     const order = document.getElementById(id)
-    totalPriceListCounter-=1
-
-    // console.log("priceOfId: ",priceOfId)
-    // console.log("priceOfId[order.id]",priceOfId[order.id])
-    // let x=priceOfId[order.id]
-    // console.log("order.id",order.id)
-    const removeItemPriceFromList = Object.entries(priceOfId).forEach(function(e){
-        if(e[0] == order.id){
-        console.log("item found: ", order.id, "Price is: ", e[1])
-        // console.log("all ammount before: ",allData)
-        // console.log("total ammount remove: ",allData-x)
-        // allData.splice(e[1],1)
-        // console.log("all ammount after: ",allData)
-
-        // totalAmmount()
+    totalPriceListCounter -= 1
+    const removeItemPriceFromList = Object.entries(priceOfId).forEach(function (e) {
+        if (e[0] == order.id) {
+            console.log("item found: ", order.id, "Price is: ", e[1])
         }
-        // console.log(allData)
-        
     })
-
-    if(totalPriceListCounter>=1){
+    if (totalPriceListCounter >= 1) {
         order.innerHTML = ''
-        
-    }else{
+    } else {
         order.innerHTML = ''
         totalEl.innerHTML = ''
         priceOfId = {}
         window.location.reload()
     }
-
 }
+
+function addToMenu(e, counter) {
+    let id = uuidv4()
+    const addItem = menuArray.filter(item => item.id == e)[0]
+    let theHtmlRender = `
+            <div class="orders" id="${id}">
+                <div class="itemAdded">
+                    <h3>${addItem.name} <!--<small class="${addItem.name}">x${counter}</small>--></h3>
+                    <button class='delete' data-delete="${id}" >remove</button>
+                    <h5>$${addItem.price}</h5>
+                </div>
+            </div>`
+    priceOfId[id] = addItem.price
+    paymentEl.innerHTML += theHtmlRender
+    totalAmmount(id, addItem.price, addItem.name, addItem.id)
+}
+
 function totalAmmount(uid, price, name, id) {
     let pair = [uid, price]
     allData.push(pair)
     console.log(allData)
-    const itemsPriceSum = allData.map(function(item){
-        return item[1]
-    })
-    .reduce(function(first, last){
-        return first+last
-    })
+    const itemsPriceSum = allData.map(function (item) {
+            return item[1]
+        })
+        .reduce(function (first, last) {
+            return first + last
+        })
     totalPriceList(itemsPriceSum)
-    
+
     // console.log(itemsPriceSum)
     // allData.push(price)
     // let totalPrice = allData.reduce(function(first, last){
@@ -113,8 +85,8 @@ function totalAmmount(uid, price, name, id) {
     // payment(totalPrice,allData,price, id, name)
 }
 
-function totalPriceList(itemPrice){
-    totalPriceListCounter+=1
+function totalPriceList(itemPrice) {
+    totalPriceListCounter += 1
     totalEl.innerHTML = `
     <hr class='hrPriceList'>
     <div class='totalPriceList'>
@@ -123,9 +95,10 @@ function totalPriceList(itemPrice){
     </div>
     <button class="submitOrder">Complete order</button>`
 }
+
 function getMenuItems() {
     return menuArray.map(e => {
-        return`
+        return `
          <div class="item">
             <p>${e.emoji}</p>
             <div class="itemType">
@@ -136,7 +109,7 @@ function getMenuItems() {
             <button data-icon="${e.id}" id="add-button" class="icon">+</button>
          </div>
          <hr>`
-         }).join('')
+    }).join('')
 }
 
 function render() {
